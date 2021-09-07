3a34db6d1822dd030a6579fded23d094e7f88e16
JoaoHAraujo <j.henr@outlook.com>
2021-09-07 17:30:20 -0300

    select-com-api

--- script.js
+++ script.js
@@ -2,36 +2,86 @@ const ufSelect = document.querySelector('#uf')
 const cidadeSelect = document.querySelector('#cidade')
 const btnSalvar = document.querySelector('#btnSalvar')
 
-const cidades = new Map()
-  cidades.set(0, ['Selecione'])
-  cidades.set(1, ['Picos', 'Teresina', 'Inhuma', 'Parnaíba', 'Oeiras'])
-  cidades.set(2, ['Recife', 'Petrolina', 'Salgueiro'])
-  cidades.set(3, ['Brasília', 'Gama', 'Taguatinga', 'Paranoá'])
+const urlUF = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
 
-ufSelect.addEventListener('change', () => { // Capta escolha de UF e popula a lista de cidades de acordo.
-  clearSelect()
-  const listaCidades = (cidades.get(parseInt(ufSelect.value)))
-  for(i = 0; i < listaCidades.length; i++) {
-    let opt = document.createElement('option')
-    opt.value = i
-    opt.text = listaCidades[i]
-    cidadeSelect.appendChild(opt)
-  }
-})
+function populaEstados() {
+  fetch(urlUF)
+    .then(res => res.json())
+    .then(estados => {
 
-function clearSelect() {  // Limpa a lista dinâmica após troca de opção de UF e coloca a opção 'selecione' como a primeira sempre.
+      estados.map(estado => {
+        const opt = document.createElement('option')
+        opt.setAttribute('value', estado.sigla)
+        opt.textContent = estado.nome
+        ufSelect.appendChild(opt)
+      })
+    })
+    .catch(() => {console.log('ERRO! A API está inoperante!')})
+}
+
+function populaCidades() {
+  ufSelect.addEventListener('change', () => { // Capta escolha de UF e popula a lista de cidades de acordo.
+    clearSelect()
+    const urlCidade = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufSelect.value}/municipios`
+
+    fetch(urlCidade)
+      .then(res => res.json())
+      .then (cidades => {
+        
+        cidades.map(cidade => {
+          const opt = document.createElement('option')
+          // opt.setAttribute('value', cidade.nome)
+          opt.textContent = cidade.nome
+          cidadeSelect.appendChild(opt)
+        })
+      .catch(() => {console.log('ERRO! A API está inoperante!')})
+      })
+  })
+}
+
+function clearSelect() {
   const clearCidadeSelect = cidadeSelect.length
   for(i = clearCidadeSelect; i > 0; i--) {
     cidadeSelect.remove(cidadeSelect[i])
   }
-  const select = document.createElement('option')
-  select.value = 0
-  select.text = cidades.get(0)
+  select = document.createElement('option')
+  select.value = ''
+  select.textContent = 'Selecione'
   cidadeSelect.appendChild(select)
 }
 
-btnSalvar.addEventListener('click', () => {  // Salvando e mostrando opções escolhidas ao apertar botão
-  const ufSelected = ufSelect.options[ufSelect.selectedIndex]
-  const cidadeSelected = cidadeSelect.options[cidadeSelect.selectedIndex]
-  window.alert(`UF: ${ufSelected.text}\nCidade: ${cidadeSelected.text}\nSalvo com sucesso!`)
-})
+populaEstados()
+populaCidades()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function clearSelect() {  // Limpa a lista dinâmica após troca de opção de UF e coloca a opção 'selecione' como a primeira sempre.
+//   const clearCidadeSelect = cidadeSelect.length
+//   for(i = clearCidadeSelect; i > 0; i--) {
+//     cidadeSelect.remove(cidadeSelect[i])
+//   }
+//   const select = document.createElement('option')
+//   select.value = 0
+//   select.text = cidades.get(0)
+//   cidadeSelect.appendChild(select)
+// }
+
+// btnSalvar.addEventListener('click', () => {  // Salvando e mostrando opções escolhidas ao apertar botão
+//   const ufSelected = ufSelect.options[ufSelect.selectedIndex]
+//   const cidadeSelected = cidadeSelect.options[cidadeSelect.selectedIndex]
+//   window.alert(`UF: ${ufSelected.text}\nCidade: ${cidadeSelected.text}\nSalvo com sucesso!`)
+// })
